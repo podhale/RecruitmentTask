84beffc5db8deb2199ba44b7ed1ce8ec6cc57f70
Andrzej <mucha.andrzej94@gmail.com>
2022-10-21 14:52:27 +0200

    Preparation of a simple api structure

---
+++ RecruitmentTask.API/Controllers/DirectoryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RecruitmentTask.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DirectoryController : ControllerBase
+    {
+        public DirectoryController()
+        {
+
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<>), StatusCodes.Status200OK)]
+        public
+    }
+}
